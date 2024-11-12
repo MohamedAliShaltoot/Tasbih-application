d4385e6eb5b8d943e93a735446c67157e54c26c4
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2024-11-12 15:51:51 +0200

    add provider state management to thr app

---
+++ flutter_application_1/lib/provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+class IncrementCounters with ChangeNotifier{
+
+ int sobhan = 0;
+  int alhamd = 0;
+  int laaaelaha = 0;
+  int alla = 0;
+  
+
+
+
+  
+}
